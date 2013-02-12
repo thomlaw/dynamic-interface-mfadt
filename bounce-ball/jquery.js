0da38c162bd9974221f636e894172143a00e9386
tom <lawrt828@newschool.edu>
2013-02-12 16:29:00 -0500

    added ball bounce

---
+++ bounce-ball/jquery.js
@@ -0,0 +1,17 @@
+
+//two separate animation functions, when aimDown completes, calls aimUp and loops through
+function animUp() {
+     $("#box").animate({
+         top: "0px" //animated begins at 0px
+     }, 1200, "swing", animDown); //loops through - length of animation and type (swing or linear)
+ }
+
+ function animDown() {
+     $("#box").animate({
+         top: "250px" //ends at 250px from top
+     }, 800, "swing", animUp);
+ }
+
+ $(document).ready(function () {
+     animUp();
+ });
